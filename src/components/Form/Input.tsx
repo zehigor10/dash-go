a480b5e28289278b76ba11218b204fca34da2ed3
José Higor <jhigorfoliveira@gmail.com>
2021-06-28 20:31:57 -0300

    Fourth commit/Form validation and autentication

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,19 +1,23 @@
 import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react";
+import { FieldError } from 'react-hook-form'
 import { forwardRef, ForwardRefRenderFunction } from "react";
+import { FormErrorMessage } from "@chakra-ui/react";
 
 interface InputProps extends ChakraInputProps{
     name: string;
     label?: string;
+    error?: FieldError;
 } 
-  const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({name, label, ...rest}, ref) => {
+  const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> 
+  = ({name, label, error = null, ...rest}, ref) => {
   return(
-        <FormControl>
+        <FormControl isInvalid={!!error}>
         { !!label && <FormLabel htmlFor={name}>{label}</FormLabel> }
 
         <ChakraInput 
           name={name} 
           id={name}
-          type="email"
+          type="string"
           focusBorderColor="pink.500"
           bgColor="gray.900"
           variant="filled"
@@ -24,6 +28,11 @@ interface InputProps extends ChakraInputProps{
           ref={ref} 
           {...rest}
         />
+        {!!error && (
+          <FormErrorMessage>
+            {error.message}
+          </FormErrorMessage>
+        )}
         </FormControl>
 
     );
